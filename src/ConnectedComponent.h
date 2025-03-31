5dd4e70859bea9f5a40ea934b022da61bb06a93f
Ethan Ngwetjana <ngweth001@myuct.ac.za>
2025-03-31 16:56:34 +0200

    Assignment3 - Implemented header file for the ConnectedComponent class

---
+++ src/ConnectedComponent.h
@@ -0,0 +1,56 @@
+/**
+ * ConnectedComponent Header File
+ * 
+ * Author: Ethan Ngwetjana
+ * Student Number: NGWETH001
+ * Date: 31/03/2025
+ * 
+ * 
+ */
+
+#include <vector>
+
+#ifndef _CC_H
+#define _CC_H
+
+class ConnectedComponent{
+private:
+    int numPixels;
+    int id;
+    std::vector<std::pair<int, int>> pairs;
+
+public:
+    //Big 6
+
+    /**
+     * Default Constructor
+     */
+    ConnectedComponent();
+
+    /**
+     * Destructor
+     */
+    ~ConnectedComponent();
+
+    /**
+     * Copy Constructor
+     */
+    ConnectedComponent(const ConnectedComponent & other);
+
+    /**
+     * Copy Assignment Operator
+     */
+    ConnectedComponent & operator=(const ConnectedComponent & other);
+
+    /**
+     * Move Constructor
+     */
+    ConnectedComponent(ConnectedComponent && rhs);
+
+    /**
+     * Move Assignment Operator
+     */
+    ConnectedComponent & operator=(ConnectedComponent && rhs);
+};
+
+#endif
