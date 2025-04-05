a36561da979843c320ba279d38f6e7b64c699dcf
Ethan Ngwetjana <ngweth001@myuct.ac.za>
2025-04-05 16:33:28 +0200

    Assignment3 - Added unit test for move constructor of the PGMimageProcessor class

--- src/UnitTests.cpp
+++ src/UnitTests.cpp
@@ -11,72 +11,116 @@
 #include "ConnectedComponent.h"
 #include "PGMimageProcessor.h"
 
-TEST_CASE("PGMimageProcessor Big Six"){
-    SECTION("Default Constructor"){
-        PGMimageProcessor p;
-        std::vector<unsigned char> inputBuffer = p.getInputBuffer();
-        int width = p.getWidth();
-        int height = p.getHeight();
-        std::vector<std::unique_ptr<ConnectedComponent>> components = p.getComponents();
-        int nextComponentID = p.getNextComponentID();
 
-        REQUIRE(inputBuffer.empty());
-        REQUIRE(width == 0);
-        REQUIRE(height == 0);
-        REQUIRE(components.empty());
-        REQUIRE(nextComponentID == 0);
+TEST_CASE("Default Constructor"){
+    PGMimageProcessor p;
+    std::vector<unsigned char> inputBuffer = p.getInputBuffer();
+    int width = p.getWidth();
+    int height = p.getHeight();
+    int numComponents = p.getComponentCount();
+    int nextComponentID = p.getNextComponentID();
 
+    REQUIRE(inputBuffer.empty());
+    REQUIRE(width == 0);
+    REQUIRE(height == 0);
+    REQUIRE(numComponents == 0);
+    REQUIRE(nextComponentID == 0);
+
+}
+
+
+
+TEST_CASE("Copy Constructor"){
+    PGMimageProcessor original;
+    
+    unsigned char data[] = {
+        255, 255, 0,
+        255, 255, 0, 
+        0, 0, 255
+    };
+    original.setImageData(data, 3, 3);
+
+    //Populate components vector and increment nextComponentID
+    int numComponents = original.extractComponents(128, 1);
+    REQUIRE(numComponents > 0);
+
+    //Ensure original has components before copying
+    REQUIRE(original.getComponentCount() > 0);
+    REQUIRE(original.getNextComponentID() > 0);
+
+    PGMimageProcessor copy(original);
+
+    REQUIRE(original.getInputBuffer() == copy.getInputBuffer());
+    REQUIRE(original.getWidth() == copy.getWidth());
+    REQUIRE(original.getHeight() == copy.getHeight());
+    REQUIRE(original.getComponentCount() == copy.getComponentCount());
+    REQUIRE(original.getNextComponentID() == copy.getNextComponentID());
+
+    //Ensure components were deeply copied
+    const std::vector<std::unique_ptr<ConnectedComponent>> & originalComponents = original.getComponents();
+    const std::vector<std::unique_ptr<ConnectedComponent>> & copiedComponents = copy.getComponents();
+
+    //check each component
+    for (int i = 0; i < originalComponents.size(); ++i){
+        REQUIRE(originalComponents[i]->getID() == copiedComponents[i]->getID());
+        REQUIRE(originalComponents[i]->getNumPixels() == copiedComponents[i]->getNumPixels());
     }
 
+    //check independency
+
+    //remove all components in orginal
+    original.filterComponentsBySize(100, 100);
+    REQUIRE(original.getComponentCount() == 0);
+
+    //check if copy is unchanged
+    REQUIRE(copy.getComponentCount() == numComponents);
 
 
-    SECTION("Copy Constructor"){
-        PGMimageProcessor original;
-        
-        unsigned char data[] = {
-            255, 255, 0,
-            255, 255, 0, 
-            0, 0, 255
-        };
-        original.setImageData(data, 3, 3);
+}
 
-        //Populate components vector and increment nextComponentID
-        int numComponents = original.extractComponents(128, 1);
-        REQUIRE(numComponents > 0);
+TEST_CASE("Move Constructor"){
+    PGMimageProcessor original;
+    
+    unsigned char data[] = {
+        255, 255, 0,
+        255, 255, 0, 
+        0, 0, 255
+    };
+    original.setImageData(data, 3, 3);
 
-        //Ensure original has components before copying
-        REQUIRE(original.getComponentCount() > 0);
-        REQUIRE(original.getNextComponentID() > 0);
+    //Populate components vector and increment nextComponentID
+    int numComponents = original.extractComponents(128, 1);
+    REQUIRE(numComponents > 0);
 
-        PGMimageProcessor copy(original);
+    PGMimageProcessor moved(std::move(original));
 
-        REQUIRE(original.getInputBuffer() == copy.getInputBuffer());
-        REQUIRE(original.getWidth() == copy.getWidth());
-        REQUIRE(original.getHeight() == copy.getHeight());
-        REQUIRE(original.getComponentCount() == copy.getComponentCount());
-        REQUIRE(original.getNextComponentID() == copy.getNextComponentID());
+    SECTION("Check if contents moved"){
+        REQUIRE(original.getInputBuffer() == moved.getInputBuffer());
+        REQUIRE(original.getWidth() == moved.getWidth());
+        REQUIRE(original.getHeight() == moved.getHeight());
+        REQUIRE(original.getComponentCount() == moved.getComponentCount());
+        REQUIRE(original.getNextComponentID() == moved.getNextComponentID());
 
-        //Ensure components were deeply copied
         const std::vector<std::unique_ptr<ConnectedComponent>> & originalComponents = original.getComponents();
-        const std::vector<std::unique_ptr<ConnectedComponent>> & copiedComponents = copy.getComponents();
+        const std::vector<std::unique_ptr<ConnectedComponent>> & movedComponents = moved.getComponents();
 
         //check each component
         for (int i = 0; i < originalComponents.size(); ++i){
-            REQUIRE(originalComponents[i]->getID() == copiedComponents[i]->getID());
-            REQUIRE(originalComponents[i]->getNumPixels() == copiedComponents[i]->getNumPixels());
+            REQUIRE(originalComponents[i]->getID() == movedComponents[i]->getID());
+            REQUIRE(originalComponents[i]->getNumPixels() == movedComponents[i]->getNumPixels());
         }
 
-        //check independency
-
-        //remove all components in orginal
-        original.filterComponentsBySize(100, 100);
-        REQUIRE(original.getComponentCount() == 0);
-
-        //check if copy is unchanged
-        REQUIRE(copy.getComponentCount() == numComponents);
-        
-
-
-
     }
+
+    SECTION("Check if original is empty"){
+        REQUIRE(original.getInputBuffer().empty());
+        REQUIRE(original.getWidth() == 0);
+        REQUIRE(original.getHeight() == 0);
+        REQUIRE(original.getComponents().empty());
+        REQUIRE(original.getNextComponentID() == 0);
+    }
+
 }
+
+
+
